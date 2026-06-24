dddab616ad5977786b9b79b514d75613ae7a3335
sans <sansbocs@gmail.com>
2026-06-23 21:10:07 -0600

    reading a file

--- main.cpp
+++ main.cpp
@@ -8,8 +8,19 @@ int main(){
 	if(!std::filesystem::exists(file_path)){
 		std::cout << "file " << file_path << " doesn't exist" << std::endl;
 	}
+	std::ifstream file_reading_stream(file_path);
+	if(!file_reading_stream.is_open()){
+		std::cout << "Error: file " << file_path << " could not be opened" << std::endl;
+	}
+	int i{1};
 	std::string s;
-	std::cin >> s;
-	std::cout << "input: " << s << std::endl;
+	std::getline(file_reading_stream, s);
+	while(std::getline(file_reading_stream, s)){
+		std::cout << "line [ " << i << "]: " << s << std::endl;
+		i++;
+	}
+	//std::string s;
+	//std::getline(file_reading_stream, s);
+	//std::cin >> s;
 	return 0;
 }
